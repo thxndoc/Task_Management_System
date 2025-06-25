0bd3abb527b94013ee19b12756f5bbc6a8c658f0
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-25 19:46:46 +0200

    Add project model with attributes

---
+++ TaskManagementSystem/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string CreatedBy { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateOnly CreatedDate { get; set; }
+    }
+}
