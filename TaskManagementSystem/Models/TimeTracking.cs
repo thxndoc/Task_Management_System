2cb12b45c2b596e1cf92485875d0e927f7105de6
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-26 12:14:54 +0200

    Add TimeTracking model with its related properties

---
+++ TaskManagementSystem/Models/TimeTracking.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class TimeTracking
+    {
+        public int TimeTrackingId { get; set; }
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public decimal HoursWorked { get; set; }
+        public DateTime WorkDate { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
