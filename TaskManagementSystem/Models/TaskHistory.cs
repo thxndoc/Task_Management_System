8909b3d74817242f522239f2d8ee27b4de4f0656
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-26 12:04:56 +0200

    Add TaskHistory model with its related properties

---
+++ TaskManagementSystem/Models/TaskHistory.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class TaskHistory
+    {
+        public int HistoryId { get; set; }
+        public int TaskId { get; set; }
+        public int ChangedByUserId { get; set; }
+        public string FieldChanged { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public string ChangeType { get; set; }
+    }
+}
