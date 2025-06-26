85702181af07dab12dd88d14160347a97e4a70bf
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-26 11:17:27 +0200

    Add Role model with Role related properties

---
+++ TaskManagementSystem/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+        public string RoleName { get; set; }
+        public string ProjectLead { get; set; }
+        public string SystemAdmin { get; set; }
+        public string Developer { get; set; }
+        public string SoftwareTester { get; set; }
+    }
+}
