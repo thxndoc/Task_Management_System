2884cfa3ece1d84d3e05d668d19bb1cae502d762
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-26 12:09:01 +0200

    Add TestReport model with its related properties

---
+++ TaskManagementSystem/Models/TestReport.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class TestReport
+    {
+        public int TestReportId { get; set; }
+        public int BugId { get; set; }
+        public string GeneratedBy { get; set; }
+        public DateOnly ReportDate { get; set; }
+        public string Details { get; set; }
+    }
+}
