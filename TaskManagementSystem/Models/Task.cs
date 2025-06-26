3a3219f4d0d28c28f33e1fd062df648e1cb00f35
RamadwaParis <ramadwaparis@gmail.com>
2025-06-26 16:53:33 +0200

    Add Models

---
+++ TaskManagementSystem/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public int ProjectId { get; set; }
+        public int AssignedToUserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public string Status { get; set; }
+    }
+}
