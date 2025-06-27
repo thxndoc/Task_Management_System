a86bab3e55e209bad022bbe4c637a14dba590423
Duma107 <dumisaninxumalo5@gmail.com>
2025-06-27 11:39:23 +0200

    Create ItaskItemService interface

---
+++ TaskManagementSystem/Services/ITaskItemService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services
+{
+    public interface ITaskItemService
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem> GetByIdAsync(int id);
+        Task CreateAsync(TaskItem taskItem);
+        Task UpdateAsync(TaskItem taskItem);
+        Task DeleteAsync(int id);
+    }
+}
+
