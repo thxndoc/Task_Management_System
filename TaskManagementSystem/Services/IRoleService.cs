558a94cdb32ef00013388949095404c17899272c
Duma107 <dumisaninxumalo5@gmail.com>
2025-06-27 11:22:05 +0200

    Create RoleService interface

---
+++ TaskManagementSystem/Services/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services
+{
+    public interface IRoleService
+    {
+        Task<IEnumerable<Role>> GetAllAsync();
+        Task<Role> GetByIdAsync(int id);
+        Task CreateAsync(Role role);
+        Task UpdateAsync(Role role);
+        Task DeleteAsync(int id);
+    }
+}
