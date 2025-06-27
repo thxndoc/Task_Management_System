5c9eae1c17e72b4ddb70920c843ce3a68c3c99cc
Duma107 <dumisaninxumalo5@gmail.com>
2025-06-27 12:08:43 +0200

    Implement Project service

---
+++ TaskManagementSystem/Services/ProjectService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementSystem.Data;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly AppDbContext _context;
+
+        public ProjectService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Project>> GetAllAsync()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        public async Task<Project> GetByIdAsync(int id)
+        {
+            return await _context.Projects.FindAsync(id);
+        }
+
+        public async Task CreateAsync(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Project project)
+        {
+            _context.Projects.Update(project);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+            if (project != null)
+            {
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
