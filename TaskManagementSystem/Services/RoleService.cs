88905755ca18ab117e582aaea86655d7bfe560b7
Duma107 <dumisaninxumalo5@gmail.com>
2025-06-27 11:22:21 +0200

    Implement Role Service

---
+++ TaskManagementSystem/Services/RoleService.cs
@@ -0,0 +1,52 @@
+﻿// Services/RoleService.cs
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementSystem.Data;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly AppDbContext _context;
+
+        public RoleService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Role>> GetAllAsync()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        public async Task<Role> GetByIdAsync(int id)
+        {
+            return await _context.Roles.FindAsync(id);
+        }
+
+        public async Task CreateAsync(Role role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Role role)
+        {
+            _context.Roles.Update(role);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if (role != null)
+            {
+                _context.Roles.Remove(role);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
