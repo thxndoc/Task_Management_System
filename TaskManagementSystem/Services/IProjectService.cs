ba7721bb6058b21cccaab773ae7a443123d796b5
Duma107 <dumisaninxumalo5@gmail.com>
2025-06-27 12:05:38 +0200

    Create IProjectService interface

---
+++ TaskManagementSystem/Services/IProjectService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<Project>> GetAllAsync();
+        Task<Project> GetByIdAsync(int id);
+        Task CreateAsync(Project project);
+        Task UpdateAsync(Project project);
+        Task DeleteAsync(int id);
+    }
+}
+
