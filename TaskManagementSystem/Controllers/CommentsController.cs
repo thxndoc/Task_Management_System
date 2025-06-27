2f41c68e214e2429af8f76a16fcc7d572cc2f7ef
Dumisani Nxumalo <Dumisani.Nxumalo@dynamicdna.co.za>
2025-06-27 09:30:18 +0200

    Initialize CommentsController with dependency injection and required namespaces

---
+++ TaskManagementSystem/Controllers/CommentsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OmnitakTaskCommandCenter.Models;
+using OmnitakTaskCommandCenter.Services;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ICommentService _service;
+        public CommentsController(ICommentService service) { _service = service; }
+    }
+}
